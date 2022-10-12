a5e5c83722e464b7a597b3d3ef7ce4c9a35bca7f
ivivix90 <wiszenkoigor@gmail.com>
2022-10-12 21:26:33 +0200

    Initial commit

---
+++ src/main/java/w/igor/server/ServermanagerApplication.java
@@ -0,0 +1,13 @@
+package w.igor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServermanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServermanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/w/igor/server/ServermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package w.igor.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServermanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
